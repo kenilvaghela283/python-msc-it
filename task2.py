72f6de92286c4bc63e00198cdef6ee200439c109
kenilvaghela283 <kenilvaghela283@gmail.com>
2026-08-06 12:05:24 +0530

    practical no.2.py
    Added comments to clarify the purpose of the code and provided an example of finding a missing roll number.

--- task2.py
+++ task2.py
@@ -1,3 +1,12 @@
+#practical no.2.py
+#Missing Roll Number
+#roll number should be from 1 to N.
+#one roll number is missing.
+#find the missing roll number without sorting.
+#example:
+#1 2 3 5 6
+#output:
+#4
 k=(1,2,3,5,6)
 for i in range(1,7):
     if i not in k:
