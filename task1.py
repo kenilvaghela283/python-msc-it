802267455dc1679d18c7435f502be6464dca4ff0
kenilvaghela283 <kenilvaghela283@gmail.com>
2026-08-06 11:59:13 +0530

    Implement consecutive duplicate detector
    Added functionality to detect and display consecutive duplicates from a list of integers.

--- task1.py
+++ task1.py
@@ -1,3 +1,12 @@
+#practical no.1.py
+#Consecutive duplicate detector
+#Accept N integers.
+#Display only those number that appear consecutively more than once.
+#inpute:
+#1 2 2 3 4 4 4 5
+#output:
+#2
+#4
 a=(1,2,2,3,4,4,4,5)
 for i in set(a):
     if a.count(i) > 1:
