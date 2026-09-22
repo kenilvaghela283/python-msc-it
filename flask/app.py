9d432bbd18037849a92b36f9c74038a50cc5a454
kenilvaghela283 <kenilvaghela283@gmail.com>
2026-09-22 23:02:09 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,28 @@
+from flask import Flask,render_template,request
+#from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return "Mahadev Mahadev"
+
+@app.route("/home",methods=["GET","POST"])
+def home():
+    print(request.method)
+
+    if request.method == "POST":
+        name = request.form.get("todo-name")
+        date = request.form.get("todo-date")
+        status = request.form.get("todo-status")
+        print(name,date,status)
+        return name
+    else:
+        # return "Invalid Data"
+
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
