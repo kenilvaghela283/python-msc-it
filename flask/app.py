93ac304b3396a5cdae3a5c55b906da5faa8de2ae
kenilvaghela283 <kenilvaghela283@gmail.com>
2026-09-24 11:54:12 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,28 +1,46 @@
-from flask import Flask,render_template,request
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from db_config import db, TODO
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 
 @app.route("/")
 def root():
-    return "Mahadev Mahadev"
+    return "<h1>hello welcom to Flask</h1>"
 
-@app.route("/home",methods=["GET","POST"])
+
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    print(request.method)
 
     if request.method == "POST":
+
         name = request.form.get("todo-name")
         date = request.form.get("todo-date")
         status = request.form.get("todo-status")
-        print(name,date,status)
-        return name
-    else:
-        # return "Invalid Data"
 
-        return render_template("index.html")
+        
+        todo = TODO(
+            name=name,
+            due_date=date,
+            status=status
+        )
+
+    
+        db.session.add(todo)
+        db.session.commit()
+
+        return "Data Stored Successfully!" 
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
